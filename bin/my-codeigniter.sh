b11c57264c1c6632bdda0510d9780ceed9329abc
kenjis <kenji.uui@gmail.com>
2015-11-13 06:58:57 +0900

    Update my-codeigniter.sh

--- bin/my-codeigniter.sh
+++ bin/my-codeigniter.sh
@@ -6,20 +6,20 @@ cd ..
 # Install translations
 php bin/install.php translations develop
 
+# Install CodeIgniter Simple and Secure Twig
+composer require kenjis/codeigniter-ss-twig:1.0.x@dev
+php vendor/kenjis/codeigniter-ss-twig/install.php
+
 # Install Codeigniter Matches CLI
 php bin/install.php matches-cli master
 
 # Install Cli for CodeIgniter
-composer require kenjis/codeigniter-cli:1.0.x@dev --dev
+composer require kenjis/codeigniter-cli --dev
 php vendor/kenjis/codeigniter-cli/install.php
 
 # Install CI PHPUnit Test
-composer require kenjis/ci-phpunit-test:1.0.x@dev --dev
+composer require kenjis/ci-phpunit-test --dev
 php vendor/kenjis/ci-phpunit-test/install.php
-
-# Install CodeIgniter Simple and Secure Twig
-composer require kenjis/codeigniter-ss-twig:1.0.x@dev
-php vendor/kenjis/codeigniter-ss-twig/install.php
 
 # Install CodeIgniter Deployer
 composer require kenjis/codeigniter-deployer:1.0.x@dev --dev
