26793e086ed6fec4d41c0bae8926fff13fd11471
Ricardo Pérez <ricpelo@gmail.com>
2015-12-21 19:03:01 +0100

    Update install.php
    Fixes wrong name string for Ion Auth.

--- bin/install.php
+++ bin/install.php
@@ -57,7 +57,7 @@ class Installer
                 'site'  => 'github',
                 'user'  => 'benedmunds',
                 'repos' => 'CodeIgniter-Ion-Auth',
-                'name'  => 'Codeigniter Matches CLI',
+                'name'  => 'Codeigniter Ion Auth',
                 'dir'   => array(
                     'config', 'controllers', 'language', 'libraries',
                     'migrations', 'models', 'sql', 'views'
