05317d08efe80ce73be4831a1c4e4da06243582e
Tetsuro Yoshikawa <tetsuro.yoshikawa@seezoo.co.jp>
2018-07-27 10:28:48 +0900

    fix: BC break

--- bin/server.sh
+++ bin/server.sh
@@ -10,4 +10,8 @@
 cd `dirname $0`
 cd ..
 
-php spark serve $@
+SERVER_HOST_AND_PORT=${1:-127.0.0.1:8000}
+SERVER_HOST=${SERVER_HOST_AND_PORT%:*}
+SERVER_PORT=${SERVER_HOST_AND_PORT##*:}
+
+php spark serve -host $SERVER_HOST -port $SERVER_PORT
